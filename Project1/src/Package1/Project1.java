66b1c353d373017562abbd76a6513a7822cd3f6a
NileshBisne <NileshBisne@DESKTOP-V8P0QL7>
2022-08-09 12:57:36 +0530

    Pushing the code first time

---
+++ Project1/src/Package1/Project1.java
@@ -0,0 +1,24 @@
+package Package1;
+
+public class Project1
+{
+	int a;
+	public void nilesh()
+	{
+		
+		System.out.println("Welcome to new API batch");
+	}
+	public static void main(String[] args)
+	{
+		Project1 abc=new Project1();
+		abc.nilesh();
+		abc.nilesh();
+		abc.a =45;
+		System.out.println(abc.a);
+		abc.a = 789;
+		abc.a = 123;
+		System.out.println(abc.a);
+		
+	}
+
+}
